f600411d91fc53c09f03e23198c079c48c84ff7c
whjin <wuhuajin09@163.com>
2024-05-14 00:09:17 +0800

    feat: 测试推送主题配置

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,11 @@ set -e
 # 构建 && 部署
 npm run deploy
 
+# 创建临时分支
+cd /d D:\ABlog\Blog\themes\anzhiyu
+git subtree split -P themes/anzhiyu -b temp-branch
+git push origin temp-branch:dev
+
 # 提交至dev分支
 git add .
 git commit -m "提交更新dev分支"
