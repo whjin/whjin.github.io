052180c58f97003787e03408ae137f55685d1f64
whjin <wuhuajin09@163.com>
2026-02-28 15:06:32 +0800

    修改配置信息

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
 
-# 构建 && 部署
+# 构建
 npm run build
+echo "构建完成"
+
+# 部署
 npm run deploy
 
 # 提交至dev分支
 git add .
-git commit -m "提交更新dev分支"
+read -p "请输入提交信息：" msg
+git commit -m "$msg"
 git push origin dev
 
 # 按任意键关闭
