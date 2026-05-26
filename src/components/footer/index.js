031e046faabb645ed3eb8d6319df34cf35a1bf73
whjin <wuhuajin09@163.com>
2026-05-26 17:35:50 +0800

    提交更新

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -12,14 +12,14 @@
       </a>&nbsp;|
       <span id="busuanzi_container_site_pv" style="display: inline">&nbsp;访问
         <a
-          href="http://service.ibruce.info/"
+          href="https://www.busuanzi.cc/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <span id="busuanzi_value_site_uv"></span>
         </a>人        
         <a
-          href="http://service.ibruce.info/"
+          href="https://www.busuanzi.cc/"
           target="_blank"
           rel="noopener noreferrer"
         >
